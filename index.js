48de76708138241abb473be3fc508d0850318b6d
Aysan Isayo <aisayo@icloud.com>
2021-07-22 19:37:20 -0700

    moved form into index.html'

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const pokeContainer = document.getElementById("poke-container");
-const pokeFormContainer = document.getElementById("poke-form-container");
+const pokeForm = document.getElementById('poke-form')
 
 function renderPoke(pokemon) {
   const pokeCard = document.createElement("div");
@@ -28,48 +28,6 @@ function renderPoke(pokemon) {
 function increaseLikes(event) {
     const likes = event.target.previousElementSibling
     likes.textContent = parseInt(likes.textContent) + 1
-    // grab the current likes number
-    // increment it 
-    // change likes element text to new #
-
-
-}
-
-function createBttn() {
-  const bttn = document.createElement("button");
-  bttn.id = "new-bttn";
-  bttn.textContent = "Add Pokemon";
-  bttn.addEventListener("click", pokeForm);
-  pokeFormContainer.appendChild(bttn);
-}
-
-function pokeForm(event) {
-  // event handler
-  const bttn = event.target;
-  bttn.remove();
-
-  const form = document.createElement("form");
-  form.id = "poke-form";
-  form.addEventListener("submit", createPoke);
-
-  const nameInput = document.createElement("input");
-  nameInput.type = "text";
-  nameInput.id = "name-input";
-  const nameLabel = document.createElement("label");
-  nameLabel.textContent = " Poke Name: ";
-
-  const imgInput = document.createElement("input");
-  imgInput.type = "text";
-  imgInput.id = "img-input";
-  const imgLabel = document.createElement("label");
-  imgLabel.textContent = " Poke img: ";
-
-  const submit = document.createElement("input");
-  submit.type = "submit";
-  submit.value = "Submit";
-
-  form.append(nameLabel, nameInput, imgLabel, imgInput, submit);
-  pokeFormContainer.appendChild(form);
 }
 
 function createPoke(event) {
@@ -82,11 +40,12 @@ function createPoke(event) {
     likes: 0,
   };
   renderPoke(poke);
+  pokeForm.reset()
 }
 
 function init() {
   pokemons.forEach(renderPoke);
-  createBttn();
+  pokeForm.addEventListener('submit', createPoke)
 }
 
 init();
