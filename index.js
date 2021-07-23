86c0161500a7354a91d0c86f216659a2d714542b
Aysan Isayo <aisayo@icloud.com>
2021-07-22 19:56:27 -0700

    organized code in renderPoke

--- index.js
+++ index.js
@@ -3,21 +3,22 @@ const pokeFormContainer = document.getElementById("poke-form-container");
 
 function renderPoke(pokemon) {
   const pokeCard = document.createElement("div");
-  const pokeImg = document.createElement("img");
-  const pokeName = document.createElement("h3");
-  const pokeLikes = document.createElement("h5");
-
   pokeCard.id = `poke-${pokemon.id}`;
   pokeCard.className = "poke-card";
+  
+  const pokeImg = document.createElement("img");
   pokeImg.src = pokemon.img;
   pokeImg.alt = `${pokemon.name} image`;
+  
+  const pokeName = document.createElement("h3");
   pokeName.textContent = pokemon.name;
+  
+  const pokeLikes = document.createElement("h5");
   pokeLikes.textContent = `Likes: ${pokemon.likes}`;
 
   pokeCard.append(pokeImg, pokeName, pokeLikes);
   pokeContainer.appendChild(pokeCard);
 }
-
 
 function init() {
   pokemons.forEach(renderPoke);
